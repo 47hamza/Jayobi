a2306459671ffec779384f5e984ce2a157e4d7ab
Noor938 <ns363780@gmail.com>
2025-03-21 23:37:59 +0500

    how works

--- src/components/HowWorks.jsx
+++ src/components/HowWorks.jsx
@@ -1,55 +1,48 @@
-// import QR from "../assets/HowWork/QR.svg";
-// import ticket from "../assets/HowWork/ticket.svg";
-// import polygon from "../assets/HowWork/polygon.svg";
+import { SiGnuprivacyguard } from "react-icons/si";
+import { FaUsers, FaTasks, FaCogs } from "react-icons/fa";
 
 const data = [
   {
-    // icon: ticket,
-    title: "Create Your Event",
-    desc: "Enter your event details and generate a claimable QR code (ccode) in minutes.",
+    icon: <SiGnuprivacyguard className="text-4xl fill-primary mx-auto mb-4" />, 
+    title: "Sign Up",
+    desc: "Create your account and personalize your dashboard in minutes.",
   },
   {
-    // icon: QR,
-    title: "Share QR Code",
-    desc: "The end users can claim the NFT tickets by scanning the QR code. We support Enjin wallet to pay and receive the tickets in real-time.",
+    icon: <FaUsers className="text-4xl fill-primary mx-auto mb-4" />, 
+    title: "Add Your Team",
+    desc: "Invite team members and set up roles, availability, and schedules.",
   },
   {
-    // icon: polygon,
-    title: "Get Paid",
-    desc: "Receive POL instantly in your ccode wallet as your audience claims the NFT tickets. It’s that simple.",
+    icon: <FaTasks className="text-4xl fill-primary mx-auto mb-4" />, 
+    title: "Start Managing",
+    desc: "Post job openings, request holidays, assign shifts, and start collaborating instantly.",
+  },
+  {
+    icon: <FaCogs className="text-4xl fill-primary mx-auto mb-4" />, 
+    title: "Streamline Operations",
+    desc: "Enjoy a more organized team with seamless integrations across all features.",
   },
 ];
 
 export default function HowWork() {
   return (
-    <section
-      className="w-full px-6 py-16 md:py-20 relative z-10"
-      id="how-it-works"
-    >
-      <div className="max-w-[1280px] md:p-6 mx-auto w-full rounded-xl md:border border-[#231F21]">
-        <h2
-          className="mt-3 text-center text-2xl md:text-4xl font-medium mb-10 !font-poppins"
-          style={{
-            background: "linear-gradient(90deg, #FFFFFF 0%, #999999 100%)",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-          }}
-        >
+    <section className="w-full px-6 py-16 md:py-20" id="how-it-works">
+      <div className="max-w-[1280px] mx-auto w-full">
+        <h2 className="mt-3 text-center text-2xl md:text-4xl font-medium !font-poppins text-primary">
           How it Works
         </h2>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-          {data?.map((itm, idx) => (
+        <p className="text-center text-sm font-light">
+          Getting started is simple. Here’s how you can begin.
+        </p>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 mt-12">
+          {data.map((itm, idx) => (
             <div
-              key={idx + 1}
-              className={`px-8 py-8 w-full relative overflow-hidden rounded-lg text-center bg-[#141414] border ${idx === 0 ? "border-primary" : "border-[#FFFFFF14]"}`}
+              key={idx}
+              className="px-8 py-8 w-full relative overflow-hidden rounded-lg text-center  bg-[#F4f4f4]"
             >
               <div className="relative z-30">
-                <img
-                  src={itm.icon}
-                  className="mx-auto d-block mb-5 w-12"
-                />
-
-                <p className="text-xl md:text-2xl mb-2.5 font-semibold">{itm.title}</p>
+                {itm.icon}
+                <p className="text-xl md:text-3xl mb-2.5 font-semibold">{itm.title}</p>
                 <div className="max-w-[300px] mx-auto">
                   <p className="mb-4">{itm.desc}</p>
                 </div>
